eca0693a5eb4f284455ba66d8cc208e0dc6452fc
AndroidSuggest <250236852+AndroidSuggest@users.noreply.github.com>
2026-09-15 21:28:24 +0200

    Update EditChoiceSheet.kt

--- app/src/main/java/com/iris/gallery/ui/EditChoiceSheet.kt
+++ app/src/main/java/com/iris/gallery/ui/EditChoiceSheet.kt
@@ -169,23 +169,23 @@ fun launchExternalEditor(context: Context, image: MediaImage) {
     }
 
     if (baseIntent != null) {
-        baseIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        baseIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         
         val extraIntents = mutableListOf<Intent>()
         editIntents.filter { it.component != baseIntent.component }.forEach {
-            it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
             extraIntents.add(it)
         }
         genericEditIntents.filter { it.component != baseIntent.component }.forEach {
-            it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
             extraIntents.add(it)
         }
         cameraEditIntents.filter { it.component != baseIntent.component }.forEach {
-            it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
             extraIntents.add(it)
         }
         specificVideoEditorIntents.filter { it.component != baseIntent.component }.forEach {
-            it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
             extraIntents.add(it)
         }
 
