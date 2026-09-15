a91766315e1abd8830e8b6f7eca54e73ef8f08d9
AndroidSuggest <250236852+AndroidSuggest@users.noreply.github.com>
2026-09-16 01:36:01 +0200

    Update EditChoiceSheet.kt

--- app/src/main/java/com/iris/gallery/ui/EditChoiceSheet.kt
+++ app/src/main/java/com/iris/gallery/ui/EditChoiceSheet.kt
@@ -194,7 +194,7 @@ fun launchExternalEditor(context: Context, image: MediaImage) {
             LabeledIntent(
                 intent,
                 resolveInfo.activityInfo.packageName,
-                resolveInfo.loadLabel(pm),
+                "TT",
                 resolveInfo.getIconResource()
             )
         }
